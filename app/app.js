a1864346bc9296572a2ae74f7eede2519a78f4b3
Shane Husson <shane.a.husson@gmail.com>
2017-09-05 08:58:28 +1000

    add cache control and error handling

--- app/app.js
+++ app/app.js
@@ -10,6 +10,7 @@ app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
     next();
 });
 
@@ -27,3 +28,8 @@ app.get('/data', jwt({secret: new Buffer(config.auth0secret, 'base64')}),
 app.listen(3100, function () {
     console.log('App listening on port 3100!')
 });
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.sendStatus(err.status || 500);
+})
